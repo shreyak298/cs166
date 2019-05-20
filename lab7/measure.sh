fafbdd93211a94eb0f0f2d27ec015346792361a6
Shreya Kumar <skuma021@ucr.edu>
2019-05-19 18:20:33 -0700

    Create measure.sh

---
+++ lab7/measure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+psql -h localhost -p $PGPORT $USER"_DB" < create_tables.sql > /dev/null
+sleep 5
+
+echo "Query time without indexes"
+cat <(echo '\timing') queries.sql |psql -h localhost -p $PGPORT $USER"_DB" | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
+
+psql -h localhost -p $PGPORT $USER"_DB" < create_indexes.sql > /dev/null
+
+echo "Query time with indexes"
+cat <(echo '\timing') queries.sql |psql -h localhost -p $PGPORT $USER"_DB" | grep Time | awk -F "Time" '{print "Query" FNR $2;}'
+
